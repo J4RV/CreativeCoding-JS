e4a76a0f87c4faddae20aa2c68da0edbbacb0866
Jarv <6jarv91@gmail.com>
2018-03-21 23:54:57 +0100

    Code cleaning for game of life

--- gameOfLife/usingGpu.js
+++ gameOfLife/usingGpu.js
@@ -1,10 +1,11 @@
 const gpu = new GPU();
+const WIDTH = 720;
+const HEIGHT = 720;
+const MS_BETWEEN_FRAMES = 16.67;
+const INITIAL_LIFE = WIDTH * HEIGHT * 0.2;
 
 var matrix;
-
-var WIDTH = 512;
-var HEIGHT = 512;
-
+var body;
 
 const initMatrix = gpu.createKernel(function(value) {  
   return value;
@@ -12,20 +13,22 @@ const initMatrix = gpu.createKernel(function(value) {
 .setOutput([HEIGHT, WIDTH]);
 
 const render = gpu.createKernel(function(matrix) {
-  var i = matrix[this.thread.y][this.thread.x];
+  var i = matrix[this.thread.y][this.thread.x] + 0.1; // +0.1 to not use pure black
   this.color(i, i, i, 1);
 })
 .setOutput([HEIGHT, WIDTH])
 .setGraphical(true);
+
+const CANVAS = render.getCanvas();
 
 function calcNeighbors(matrix, x, y, width, height) {
   var up =    (y+1)%height;
   var down =  (y-1)%height;
   var right = (x+1)%width;
   var left =  (x-1)%width;
-  return matrix[left ][up  ] + matrix[left ][y] + matrix[left ][down] + 
-         matrix[x    ][up  ]          +           matrix[x    ][down] +
-         matrix[right][up  ] + matrix[right][y] + matrix[right][down];
+  return matrix[left ][up] + matrix[left ][y] + matrix[left ][down] + 
+         matrix[  x  ][up]          +           matrix[  x  ][down] +
+         matrix[right][up] + matrix[right][y] + matrix[right][down];
 }
 gpu.addFunction(calcNeighbors);
 
@@ -43,52 +46,24 @@ const lifeGame = gpu.createKernel(function(matrix) {
 .setOutput([HEIGHT, WIDTH])
 .setOutputToTexture(true);
 
-const canvas = render.getCanvas();
-var body;
 function gameOfLifeUpdate() {
   matrix = lifeGame(matrix);
   render(matrix);
-  setTimeout(gameOfLifeUpdate, 16);
+  setTimeout(gameOfLifeUpdate, MS_BETWEEN_FRAMES);
 }
+
 
 $(document).ready(function() {
   matrix = initMatrix(0);
-  render(matrix);
   body = document.getElementsByTagName('body')[0];
-  body.appendChild(canvas);
-  console.log(canvas);
+  body.appendChild(CANVAS);
   
-  /*matrix[0][2] = 1;
-  matrix[1][1] = 1;
-  matrix[0][0] = 1;
-  matrix[2][2] = 1;
-  console.log(matrix);
-  */
-  
-  matrix[10+0][10+0] = 1;
-  matrix[10+0][10+1] = 1;
-  matrix[10+0][10+2] = 1;
-  matrix[10+4][10+2] = 1;
-  matrix[10+1][10+2] = 1;
-  matrix[10+2][10+1] = 1;
-  matrix[10+3][10+1] = 1;
-  matrix[10+4][10+1] = 1;
-  matrix[10+4][10+2] = 1;
-  matrix[10+3][10+3] = 1;
-  var i = 20000;
-  while(i-- > 0){
+  let i = INITIAL_LIFE;
+  while(i --> 0){
 	  let x = Math.floor(Math.random() * HEIGHT);
 	  let y = Math.floor(Math.random() * WIDTH);
 	  matrix[x][y] = 1;
   }
-  console.log(matrix);
   
-  
-  console.log(calcNeighbors(matrix, 1, 1, WIDTH, HEIGHT));
-  //matrix = lifeGame(matrix);
-  console.log(matrix);
-  console.log(calcNeighbors(matrix, 1, 1, WIDTH, HEIGHT));
-  
-  
-  setTimeout(gameOfLifeUpdate, 16);
+  gameOfLifeUpdate();
 });
